92cc1e5f34e48cc940f09f24808145d9ad606380
jacksonk <jacksonk@bcr.co.za>
2025-04-24 16:40:58 +0200

    404 without redirect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home"; // Corrected import path
-import Register from "./pages/register"; // Corrected import path
-import Login from "./pages/login";   // Corrected import path
-import NotFound from "./pages/notFound"; // Corrected import path
+import Home from "./pages/home";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import NotFound from "./pages/notFound";
 import Header from './components/header';
 import Footer from "./components/footer"; // Import Footer
 
 function App() {
   return (
-    <Router>
+    <Router basename="/invoicing-system-web-app"> {/* Add basename */}
       <div className="flex flex-col min-h-screen">
         <Header /> {/* Include Header */}
         <main className="flex-grow">
